f92f70960fbd93262ed39c85aaa83173d8d6278d
Eli <136011323+EliasElliotson@users.noreply.github.com>
2025-02-26 02:27:02 +0000

    Made the title into an optional parameter in Page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,7 @@
 function Page({ title, children }) {
   return (
     <main class="body-column">
-        <h1>{ title }</h1>
+        {title && <h1>{ title }</h1>}
         {children}
     </main>
   );
